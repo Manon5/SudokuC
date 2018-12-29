e5cd5ce202f30375bd8f2b518f9eb5336a0839d4
FLedeux <44844255+FLedeux@users.noreply.github.com>
2018-12-29 15:46:16 +0100

    Update initJeu.c

--- initJeu.c
+++ initJeu.c
@@ -2,19 +2,29 @@
 
 void initJeu(int G[][]*){
  
- int i, j,a;
- c=0;
+ int i, j,a,b;
+ 
+ a=0;
   for(i=0;i<9;i++)
   {
     for(j=0;j<9;j++)
     {
-      C[i][j].nbc=0;
-      C[i][j].tab=NULL;
+      *C[i][j].nbc=0;
+      *C[i][j].tab=NULL;
       if(*G[i][j]=0)
       {
+       c=0;
        *O[c].x=i;
        *O[c].y=j;
        a++;
+       for(b=1;b<10;b++){
+        if(estCand()=1)
+        {
+         *C[i][j].nbc++;
+         *C[i][j].*tab[c]=b;
+         c++;
+        }
+      }
     }
   }
  *NBO=a;
