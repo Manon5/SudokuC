0e9c2214d9dbeb5bc29c9f0f6381b37b959362cf
FLedeux <44844255+FLedeux@users.noreply.github.com>
2018-12-29 15:52:48 +0100

    Update initJeu.c

--- initJeu.c
+++ initJeu.c
@@ -1,27 +1,30 @@
 #include <studio.h>
 
-void initJeu(int G[][]*){
+void initJeu(int G[][]*)
+{
  
- int i, j,a,b;
+ int i, j,a,b,c;
  
  a=0;
   for(i=0;i<9;i++)
   {
     for(j=0;j<9;j++)
     {
-      if(*G[i][j]=0)
+      if(*G[i][j]=0)   \\détermine si la case est ouverte ou femré
       {
        c=0;
        *O[c].x=i;
        *O[c].y=j;
        a++;
-       for(b=1;b<10;b++){
-        if(estCand()=1)
+       for(b=1;b<10;b++)
+       {
+        if(estCand()=1) \\paramètres a rejouter après que la fonction est faite et determine si b est un nombre candidat
         {
          *C[i][j].nbc++;
          *C[i][j].*tab[c]=b;
          c++;
         }
+       }
       }
       else
       {
