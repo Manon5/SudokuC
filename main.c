53370e19a26820ed2fe5fb8dbf8c6e074f302748
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2018-12-29 15:06:36 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include stdio.h
+
+int main()
+{
+void lireGrille();
+void ecrireGrille();
+void initJeu();
+estCand();
+appartient();
+estCandUnique();
+rechCaseUnique();
+fermerCase();
+fermerGrille();
+ecrireCand();
+}
