6cb80eb77d75d64a028e435aa944e116df0d6fc4
Manon5 <manon.touba05@gmail.com>
2018-12-29 14:33:59 +0100

    Update lireGrille.c

--- lireGrille.c
+++ lireGrille.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
 lireGrille(){
 
+    FILE* fichier = NULL;
+
+    fichier = fopen("grille_test.txt", "r");
+
+
+    if (fichier != NULL)
+
+    {
+
+        // On peut lire et écrire dans le fichier
+
+    }
+
+    else
+
+    {
+
+        // On affiche un message d'erreur si on veut
+
+        printf("Impossible d'ouvrir le fichier");
+
+    }
+
+
+    return 0;
 
 }
