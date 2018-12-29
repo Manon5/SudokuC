d58f756202647a1cecc4082322825ec937e82e65
Manon5 <manon.touba05@gmail.com>
2018-12-29 16:00:36 +0100

    Update lireGrille.c

--- lireGrille.c
+++ lireGrille.c
@@ -1,41 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-
-
-
 void lireGrille(int *G[][]){
 // module
-    
+
     // On initialise à 0
-    int i, j;
+    int i, j, l, c, v;
     for(i = 0, i < 9, i++){
         for(j = 0, j < 9, j++){
             G[i, j] = 0;
         }
     }
-    
+
     // On lit dans le fichier texte pour remplir la grille
     FILE* fichier = NULL;
-
     fichier = fopen("grille_test.txt", "r");
 
-
-    if (fichier != NULL)
-
-    {
-
+    if (fichier != NULL){
         // Lecture du fichier
-
+        while(getchar() != EOF){
+          fscanf(fichier, "%d %d %d", &l, &c, &v);
+          G[l][c] = v;
+        }        
     }
 
-    else
-
-    {
-
+    else{
         // Erreur dans le cas où le fichier n'existe pas
 
         printf("Impossible d'ouvrir le fichier");
-
     }
 
 
