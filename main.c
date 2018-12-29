bd3d2bea6c39306a6c56f6bf062e7882e0edf19d
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2018-12-29 16:04:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
+#define TAILLE 9
 typedef struct { //modifier les x et y si utilisation d'autres noms de variables
 int x; // no de ligne
 int y; // no de colonne
@@ -11,23 +13,15 @@ int nbc; // no de candidats
 int * tab; // table candidats
 } Cand;
 
+int G[TAILLE][TAILLE];
+Case O[81];
+Cand C[9][9];
 int NBO;
-int C[i][j]={};
-int O[81];
-
-//aussis supprimer certaines fonctions si elles n'ont pas lieu d'être ici + ajouter les variables dedans 
+int lireGrille();
+void ecrireGrille()
 
 
-void lireGrille(int tab[], unsigned taille); //module de lecture du tableau G
-void ecrireGrille(); //module qui permet d'afficher le tableau G à l'écran
-void initJeu(); //module d'initialisation des tables C, O et de la variable NBO
-estCand(); //permet de savoir si un nombre est candidat(1) ou non(0)
-appartient(); //le nombre appartient-il aux nombres candidats?
-estCandUnique(); //le candidat est-il unique ou non?
-rechCaseUnique(); //cette case contient-elle un candidat unique?
-fermerCase(); //rempli une case ouverte de la grille et 
-fermerGrille(); //rempli grille par méthode des candidatis uniques
-ecrireCand(); //affiche les candidats des cases ouvertes
+
 
 int main()
 {
