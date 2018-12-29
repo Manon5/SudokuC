113884462287ea0463f5ec99dda5707262d06b71
FLedeux <44844255+FLedeux@users.noreply.github.com>
2018-12-29 15:14:18 +0100

    Update initJeu.c

--- initJeu.c
+++ initJeu.c
@@ -2,12 +2,15 @@
 
 void initJeu{
  
- int i, j;
+ int i, j,c;
+ c=0;
   for(i=0;i<9;i++)
   {
     for(j=0;j<9;j++)
     {
-      *G[i][j]=0;
+      if(*G[i][j]=0)
+       c++;
     }
   }
+ *NBO=c;
 }
