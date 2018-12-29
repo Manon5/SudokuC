e377f8860c2ac9a949bf5e8335902832deb9d2f7
Manon5 <manon.touba05@gmail.com>
2018-12-29 14:58:56 +0100

    Update lireGrille.c

--- lireGrille.c
+++ lireGrille.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int G[9] [9] //tableau à utiliser (je l'ai mis là pour ne pas oublier son initialisation)
 
-void lireGrille(){
+
+void lireGrille(int *G[][]){
 // module
+    
+    // On initialise à 0
+    int i, j;
+    for(i = 0, i < 9, i++){
+        for(j = 0, j < 9, j++){
+            G[i, j] = 0;
+        }
+    }
+    
+    // On lit dans le fichier texte pour remplir la grille
     FILE* fichier = NULL;
 
     fichier = fopen("grille_test.txt", "r");
