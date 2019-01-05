75e8f795061fb382032aede17358f25d5269a228
Manon5 <manon.touba05@gmail.com>
2019-01-05 21:39:41 +0100

    Update estCand.c

--- estCand.c
+++ estCand.c
@@ -1,28 +1,54 @@
 #include <stdio.h>
 
+
+int quelleZone(int colonne, int ligne){
+  // determine a quelle zone appartient une case (utilise dans estVoisine)
+  int zone = 0;
+  
+  // en fonction de la colonne
+  if(colonne < 3){
+    zone =  1;
+  }else if(colonne < 6){
+    zone = 2;
+  }else{
+    zone = 3;
+  }
+  
+  // En fonction de la ligne
+  if(ligne > 5){
+    zone = zone + 6;
+  }else if(ligne > 3){
+    zone  = zone + 3;
+  }
+  
+  // on retourne le resultat
+  return zone;
+  
+}
+
 int estVoisine(int cA, int lA, int cB, int lB){  
-// determine si deux cases sont voisines (utilise dans estCand)
-// cA = colonne de la case A, lA = ligne de la case A
+  // determine si deux cases sont voisines (utilise dans estCand)
+  // cA = colonne de la case A, lA = ligne de la case A
 
-int res = 0; //initialisation du resultat
+  int res = 0; //initialisation du resultat
 
-//on teste si A et B sont dans la meme ligne
-if(lA == lB){
-  res = 1; // la case est voisine
-}
-// on teste si A et B sont dans la meme colonne
-else if(cA == cB){
-  res = 1; // la case est voisine
-}
-// on teste si A et B sont dans la meme zone
+  //on teste si A et B sont dans la meme ligne
+  if(lA == lB){
+    res = 1; // la case est voisine
+  }
+  // on teste si A et B sont dans la meme colonne
+  else if(cA == cB){
+    res = 1; // la case est voisine
+  }
+  // on teste si A et B sont dans la meme zone
 
 
-// on retourne le resultat
-return res;
+  // on retourne le resultat
+  return res;
 
 }
 
 int estCand(){ 
-// determine si un nombre est candidat pour une case donnee
+  // determine si un nombre est candidat pour une case donnee
 
 }
