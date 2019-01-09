04103e01b6e8fb6fea5506e35d2469344c201e47
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2019-01-09 09:20:12 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -204,3 +204,27 @@ int estCandUnique(Cand C[][9], int x, int y, int nb){
 }
 
          
+Case rechCaseUnique(Cand C[][9]){
+  Case coor;
+  int i, j, n;
+  coor.x=NULL;
+  coor.y=NULL;
+  for(i=0;i<9;i++){
+    for(j=0;j<9;j++){
+      if (C[i][j].nbc=!0){
+        for(n=1;n<10;n++){
+          if(estCandUnique(C[9][9],i,j,n)==1){
+            coor.x=i;
+            coor.y=j;
+            return(coor);
+          }
+        }
+      }
+    }
+  }
+
+  return(coor);
+}
+         
+         
+         
