b3f3dde67cc62461a075b9da7f2b3976ebf179b5
FLedeux <44844255+FLedeux@users.noreply.github.com>
2019-01-09 08:44:44 +0100

    Update appartient.c

--- appartient.c
+++ appartient.c
@@ -1,7 +1,7 @@
 int appartient(Cand *C[][], int x, int y, int nb){
   int i;
-  for(i=0;i<*C[x][y].nbc;i++){
-    if(nb=*C[x][y].tab[i])
+  for(i=0;i<(*C)[x][y].nbc;i++){
+    if(nb=(*C)[x][y].tab[i])
       return(1);
   }
   return(0);
