3f9afcf6626b0f8e699879346710ab4fbc81f6c1
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2019-01-09 08:34:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ void initJeu(int*);
 int quelleZone(int, int); //fonction utilisée dans estVoisine
 int estVoisine(int, int, int, int, int); //fonction utilisée dans estCand 
 int estCand(int, int, int, int);
-int appartient();
+int appartient(Cand, int, int, int);
 int estCandUnique();
 int rechCaseUnique();
 int fermerCase();
