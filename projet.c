8efe61aac4fbe843e222f1d52ff007228dea900c
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2019-01-09 09:55:33 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -191,21 +191,21 @@ int appartient(Cand C[][], int x, int y, int nb){
   int i;
   for(i=0;i<C[x][y].nbc;i++){
     if(nb=C[x][y].tab[i])
-      return(1);
+      return(1); //si le chiffre appartient à l'ensemble des candidats
   }
-  return(0);
+  return(0); //si le chiffre n'appartient pas aux candidats
 }
        
          
 int estCandUnique(Cand C[][9], int x, int y, int nb){
     if((C[x][y].nbc=!1)||(C[x][y].tab[0]=!nb))
-    return(0);
-    return(1);
+    return(0); //dans le cas où il n'existe pas de condidat unique
+    return(1); //dans le cas où il existe un candidat unique
 }
 
          
 Case rechCaseUnique(Cand C[][9]){
-  Case coor;
+  Case coor; 
   int i, j, n;
   coor.x=NULL;
   coor.y=NULL;
@@ -216,18 +216,53 @@ Case rechCaseUnique(Cand C[][9]){
           if(estCandUnique(C[9][9],i,j,n)==1){
             coor.x=i;
             coor.y=j;
-            return(coor);
+            return(coor); //coordonnées de la case
           }
         }
       }
     }
   }
 
-  return(coor);
+  return(coor); //coordonnée de la case
 }
          
          
-//fonction permettant de supprimer 
+//fonction permettant de supprimer le candidat des cases adjacentes à la case remplie par ce dernier (utilisée dans la fonction fermerCase)
          
          
- 
+ void fermerCase(int *G[][9], Cand *C[][9], Case coor,int nb){
+  int i, part, j;
+  Case coor2;
+  coor2.x=0;
+  coor2.y=0;
+  *G[coor.x][coor.y]=nb;
+  (*C)[coor.x][coor.y].nbc=0;
+  (*C)[coor.x][coor.y].tab=NULL;
+  for(i=0;i<9;i++){
+    if(appartient(C[9][9], i, coor.y, nb)==1){
+      suppr(*C[9][9], i, coor.y, nb);  //fonction a faire : supprime la case du nombre dans le tableau et décale vers la droite c'elle après et fait nbc-1
+    }
+  }
+  for(i=0;i<9;i++){
+    if(appartient(C[9][9], coor.x, i, nb)==1){
+      suppr(*C[9][9], coor.x, i, nb);
+    }
+  }
+  part=quelleZone(coor.y, coor.x);
+  switch(part){
+    case 2:coor.y+=3; break;
+    case 3:coor.y+=6; break;
+    case 4:coor.x+=3; break;
+    case 5:coor.y+=3;coor.x+=3; break;
+    case 6:coor.y+=6;coor.x+=3; break;
+    case 7:coor.x+=6; break;
+    case 8:coor.y+=3;coor.x+=6; break;
+    case 9:coor.y+=6;coor.x+=6; break;
+  }
+  for (i=0;i<3;i++){
+    for(j=0;j<3;j++){
+      if(appartient(C[9][9], coor.x + i, coor.y + j, nb)==1){
+        suppr(*C[9][9], coor.x + i, coor.y + j, nb);
+      }
+    }
+  }
