58e5d5c8855c9e8f958df8bba3ea5a4aa60b6032
FLedeux <44844255+FLedeux@users.noreply.github.com>
2019-01-09 09:18:52 +0100

    Update rechCaseUnique.c

--- rechCaseUnique.c
+++ rechCaseUnique.c
@@ -1,1 +1,21 @@
+Case rechCaseUnique(Cand C[][9]){
+  Case coor;
+  int i, j, n;
+  coor.x=NULL;
+  coor.y=NULL;
+  for(i=0;i<9;i++){
+    for(j=0;j<9;j++){
+      if (C[i][j].nbc=!0){
+        for(n=1;n<10;n++){
+          if(estCandUnique(C[9][9],i,j,n)==1){
+            coor.x=i;
+            coor.y=j;
+            return(coor);
+          }
+        }
+      }
+    }
+  }
 
+  return(coor);
+}
