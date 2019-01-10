e35fcd7342e83156d959de2e6aff6d12acf1ab27
Manon5 <manon.touba05@gmail.com>
2019-01-10 08:38:10 +0100

    Update ecrireGrille.c

--- ecrireGrille.c
+++ ecrireGrille.c
@@ -1,25 +1,27 @@
+// Fonction finie et vérifiée : tout fonctionne
+
 #include <stdio.h>
 
 void ecrireGrille(int G[9][9]){
 
   int i, j;
 
-  printf("-----------------------\n");
+  printf("-------------------------\n");
 
   for(i = 0; i < 9; i++){
     printf("| ");
     for(j = 0; j < 9; j++){
       printf("%d ", G[i][j]);
       if(j == 2 || j == 5){
-        printf("|");
+        printf("| ");
       }
     }
     printf("|\n");
     if(i == 2 || i == 5){
-      printf("-----------------------\n");
+       printf("|-------|-------|-------|\n");
     }
   }
 
-  printf("-----------------------\n");
+   printf("-------------------------\n");
 
 }
