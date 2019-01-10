38402632b64798a738ed24813b8946b34adb6c14
Manon5 <manon.touba05@gmail.com>
2019-01-10 08:28:32 +0100

    Update ecrireGrille.c

--- ecrireGrille.c
+++ ecrireGrille.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
-void ecrireGrille(int *G[][]){
+void ecrireGrille(int G[9][9]){
 
   int i, j;
-  
+
   printf("------------------------------\n");
-  
-  for(i = 0, i < 9, i++){
+
+  for(i = 0; i < 9; i++){
     printf("| ");
-    for(j = 0, j < 9, j++){
+    for(j = 0; j < 9; j++){
       printf("%d ", G[i][j]);
     }
-    printf("|");
+    printf("|\n");
   }
-  
-  printf("------------------------------\n");
-  
-}
 
+  printf("------------------------------\n");
+
+}
