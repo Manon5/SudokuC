d9c79f44f162c2bd31a4ec72c7a639313bafc17d
FLedeux <44844255+FLedeux@users.noreply.github.com>
2019-01-12 17:36:22 +0100

    Update fermerCase.C

--- fermerCase.C
+++ fermerCase.C
@@ -1,21 +1,21 @@
 
 // manque encore a retirer les coordoner de O et a décaler les coordonnées qui sont à droite de la case supprimé 
-void fermerCase(int *G[][9], Cand *C[][9], Case coor,int nb){
+void fermerCase(int G[9][9], Cand C[9][9], Case coor,int nb){
   int i, part, j;
   Case coor2;
   coor2.x=0;
   coor2.y=0;
-  *G[coor.x][coor.y]=nb;
-  (*C)[coor.x][coor.y].nbc=0;
-  (*C)[coor.x][coor.y].tab=NULL;
+  G[coor.x][coor.y]=nb;
+  C[coor.x][coor.y].nbc=0;
+  C[coor.x][coor.y].tab=NULL;
   for(i=0;i<9;i++){
     if(appartient(C[9][9], i, coor.y, nb)==1){
-      suppr(*C[9][9], i, coor.y, nb);  //fonction a faire : supprime la case du nombre dans le tableau et décale vers la droite c'elle après et fait nbc-1
+      suppr(C[9][9], i, coor.y, nb);  //fonction a faire : supprime la case du nombre dans le tableau et décale vers la droite c'elle après et fait nbc-1
     }
   }
   for(i=0;i<9;i++){
     if(appartient(C[9][9], coor.x, i, nb)==1){
-      suppr(*C[9][9], coor.x, i, nb);
+      suppr(C[9][9], coor.x, i, nb);
     }
   }
   part=quelleZone(coor.y, coor.x);
@@ -32,7 +32,7 @@ void fermerCase(int *G[][9], Cand *C[][9], Case coor,int nb){
   for (i=0;i<3;i++){
     for(j=0;j<3;j++){
       if(appartient(C[9][9], coor.x + i, coor.y + j, nb)==1){
-        suppr(*C[9][9], coor.x + i, coor.y + j, nb);
+        suppr(C[9][9], coor.x + i, coor.y + j, nb);
       }
     }
   }
