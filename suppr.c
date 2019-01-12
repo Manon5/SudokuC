d2a03fefa42ca526fa6080a927736d68dd3e40cd
FLedeux <44844255+FLedeux@users.noreply.github.com>
2019-01-12 17:34:39 +0100

    Update suppr.c

--- suppr.c
+++ suppr.c
@@ -1,15 +1,15 @@
 
-void suppr(Cand *C[][9], int x, int y, int nb ){
+void suppr(Cand C[9][9], int x, int y, int nb ){
   int i,t;
-  for(i=0;i<(*C)[x][y].nbc;i++){
-    if((*C)[x][y].tab[i]==nb){
-      (*C)[x][y].tab[i]=NULL;
+  for(i=0;i<C[x][y].nbc;i++){
+    if(C[x][y].tab[i]==nb){
+      C[x][y].tab[i]=NULL;
       t=i;
       break;
     }
   }
-  for(i=t;i<(*C)[x][y].nbc-1;i++){
-    (*C)[x][y].tab[i]=(*C)[x][y].tab[i+1];
+  for(i=t;i<C[x][y].nbc-1;i++){
+    C[x][y].tab[i]=C[x][y].tab[i+1];
   }
-  (*C)[x][y].nbc--;
+  C[x][y].nbc--;
 }
