9eda0dfce39021290e6a508491b0ea44b919abdb
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2019-01-13 20:47:29 +0100

    rajout de toutes les fonctions finies et vérifiées
    manque juste la fonction ecrireCand

--- projet.c
+++ projet.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "types.h"
 
 //définition de la taille des grilles (9x9)
 #define TAILLE 9
@@ -17,19 +18,41 @@ int nbc; // no de candidats
 int * tab; // table candidats
 } Cand;
 
+//initialisation des fonctions, variables et tableaux
+int tab[9]
+int G[TAILLE][TAILLE];
+Case O[81];
+Cand C[9][9];
+int NBO;
+void lireGrille();
+void ecrireGrille();
+void initJeu(int*);
+int quelleZone(int, int); //fonction utilisée dans estVoisine
+int estVoisine(int, int, int, int, int); //fonction utilisée dans estCand 
+int estCand(int, int, int, int);
+int appartient(Cand, int, int, int);
+int estCandUnique(Cand, int, int, int);
+Case rechCaseUnique(Cand);
+void suppr(); //module utilisé dans le module fermerCase
+void fermerCase();
+void fermerGrille();
+int ecrireCand();
 
+int main()
+{
+void lireGrille();
+void ecrireGrille();
+}
 
-
-
-
-void lireGrille(int *G[][]){
+void lireGrille(int G[9][9]){
 // module
 
     // On initialise à 0
     int i, j, l, c, v;
-    for(i = 0, i < 9, i++){
-        for(j = 0, j < 9, j++){
-            G[i, j] = 0;
+    char tab[20];
+    for(i = 0; i < 9; i++){
+        for(j = 0; j < 9; j++){
+            G[i][j] = 0;
         }
     }
 
@@ -37,44 +60,49 @@ void lireGrille(int *G[][]){
     FILE* fichier = NULL;
     fichier = fopen("grille_test.txt", "r");
 
+
     if (fichier != NULL){
         // Lecture du fichier
-        while(getchar() != EOF){
-          fscanf(fichier, "%d %d %d", &l, &c, &v);
+        while(fgets(tab, 20, fichier) != NULL){
+          sscanf(tab, "%d %d %d", &l, &c, &v);
           G[l][c] = v;
-        }        
+        }
     }
 
     else{
         // Erreur dans le cas où le fichier n'existe pas
 
-        printf("Impossible d'ouvrir le fichier");
+        printf("Impossible d'ouvrir le fichier\n");
     }
 
 
 }
 
-
-void ecrireGrille(int *G[][]){
+void ecrireGrille(int G[9][9]){
 
   int i, j;
-  
-  printf("------------------------------\n");
-  
-  for(i = 0, i < 9, i++){
+
+  printf("-------------------------\n");
+
+  for(i = 0; i < 9; i++){
     printf("| ");
-    for(j = 0, j < 9, j++){
+    for(j = 0; j < 9; j++){
       printf("%d ", G[i][j]);
+      if(j == 2 || j == 5){
+        printf("| ");
+      }
     }
-    printf("|");
+    printf("|\n");
+    if(i == 2 || i == 5){
+       printf("|-------|-------|-------|\n");
+    }
   }
-  
-  printf("------------------------------\n");
-  
+
+   printf("-------------------------\n");
+
 }
 
-
-void initJeu(int *G[][])
+void initJeu(int G[9][9], Cand C[9][9], int *NBO, Case O[81])
 {
  //module
  int i, j,a,b,c;
@@ -84,39 +112,38 @@ void initJeu(int *G[][])
   {
     for(j=0;j<9;j++)
     {
-      if(*G[i][j]=0)   //détermine si la case est ouverte ou fermee
+      if(G[i][j]=0)   //détermine si la case est ouverte ou fermee
       {
        // On remplit la table O (case ouverte/fermee)
        c=0;
-       *O[c].x=i;
-       *O[c].y=j;
+       O[c].x=i; // O est de type case, si la cas est ouverte, O enregistre les coordonnée de la case
+       O[c].y=j;
        a++;
        for(b=1;b<10;b++)
        {
-        if(estCand(j,i,*G[][],b)=1) //determine si b est un nombre candidat
+        if(estCand(j,i,G[][],b)=1) //determine si b est un nombre candidat
         {
          // On remplit la table C (liste des candidats)
-         *C[i][j].nbc++;
-         *C[i][j].*tab[c]=b;
+         C[i][j].nbc++;
+         C[i][j].*tab[c]=b;
          c++;
         }
        }
       }
       else
       {
-       *C[i][j].nbc=0;
-       *C[i][j].tab=NULL;
+       C[i][j].nbc=0;
+       C[i][j].tab=NULL;
       } 
     }
   }
  *NBO=a; // Compteur de cases ouvertes
 }
 
-
 int quelleZone(int colonne, int ligne){
   // determine a quelle zone appartient une case (utilise dans estVoisine)
   int zone = 0;
-  
+
   // en fonction de la colonne
   if(colonne < 3){
     zone =  1;
@@ -125,21 +152,20 @@ int quelleZone(int colonne, int ligne){
   }else{
     zone = 3;
   }
-  
+
   // En fonction de la ligne
   if(ligne > 5){
     zone = zone + 6;
   }else if(ligne > 3){
     zone  = zone + 3;
   }
-  
+
   // on retourne le resultat
   return zone;
-  
+
 }
 
-
-int estVoisine(int cA, int lA, int cB, int lB){  
+int estVoisine(int cA, int lA, int cB, int lB){
   // determine si deux cases sont voisines (utilise dans estCand)
   // cA = colonne de la case A, lA = ligne de la case A
 
@@ -154,113 +180,104 @@ int estVoisine(int cA, int lA, int cB, int lB){
     res = 1; // la case est voisine
   }
   // on teste si A et B sont dans la meme zone
-  else if(quelleZone(cA,lA) == quelleZone(cB, lB){
+  else if(quelleZone(cA,lA) == quelleZone(cB, lB)){
     res = 1;
   }
-          
-          
+
+
   // on retourne le resultat
   return res;
 
 }
 
-          
-int estCand(int col, int lig, int *G[][], int nb){ 
+int estCand(int col, int lig, int G[9][9], int nb){
   // determine si un nombre nb est candidat pour une case donnee
 
-  
+
   int i, j; // On parcourt toute la grille
   int res = 1; // on initialise à VRAI
-  
-  for(i = 0, i < 9, i++){
-    for(j = 0, j < 9, j++){
-      if(estVoisine(i, j, col, lig){
+
+  for(i = 0; i < 9; i++){
+    for(j = 0; j < 9; j++){
+      if(estVoisine(i, j, col, lig)){
         // Les cases sont voisines : on teste si la case vaut nb
         if(G[i][j] == nb){
           // Meme valeur que nb : nb n'est pas candidat
           res = 0;
+        }
       }
     }
   }
-  return res;       
-  
+  return res;
+
 }
-         
-         
+
 int appartient(Cand C[][], int x, int y, int nb){
   int i;
   for(i=0;i<C[x][y].nbc;i++){
     if(nb=C[x][y].tab[i])
-      return(1); //si le chiffre appartient à l'ensemble des candidats
+      return(1);
   }
-  return(0); //si le chiffre n'appartient pas aux candidats
-}
-       
-         
-int estCandUnique(Cand C[][9], int x, int y, int nb){
-    if((C[x][y].nbc=!1)||(C[x][y].tab[0]=!nb))
-    return(0); //dans le cas où il n'existe pas de condidat unique
-    return(1); //dans le cas où il existe un candidat unique
+  return(0);
 }
 
-         
+int estCandUnique(Cand C[][9], int x, int y, int nb){
+    if((C[x][y].nbc=!1)||(C[x][y].tab[0]=!nb))
+    return(0);
+    return(1);
+}
+
+// a changer en utilisant O
 Case rechCaseUnique(Cand C[][9]){
-  Case coor; //création d'une structure pour les coordonnées des cases
+  Case coor;
   int i, j, n;
   coor.x=NULL;
   coor.y=NULL;
   for(i=0;i<9;i++){
     for(j=0;j<9;j++){
-      if (C[i][j].nbc=!0){
+      if (C[i][j].nbc==1){
         for(n=1;n<10;n++){
           if(estCandUnique(C[9][9],i,j,n)==1){
             coor.x=i;
             coor.y=j;
-            return(coor); //coordonnées de la case
+            return(coor);
           }
         }
       }
     }
   }
 
-  return(coor); //coordonnée de la case
+  return(coor);
 }
-         
-         
-//fonction permettant de supprimer le candidat des cases adjacentes à la case remplie par ce dernier (utilisée dans la fonction fermerCase)
- void suppr(Cand *C[][9], int x, int y, int nb ){
+
+void suppr(Cand C[9][9], int x, int y, int nb ){
   int i,t;
-  for(i=0;i<(*C)[x][y].nbc;i++){
-    if((*C)[x][y].tab[i]==nb){
-      (*C)[x][y].tab[i]=NULL;
+  for(i=0;i<C[x][y].nbc;i++){
+    if(C[x][y].tab[i]==nb){
+      C[x][y].tab[i]=NULL;
       t=i;
       break;
     }
   }
-  for(i=t;i<(*C)[x][y].nbc-1;i++){
-    (*C)[x][y].tab[i]=(*C)[x][y].tab[i+1];
+  for(i=t;i<C[x][y].nbc-1;i++){
+    C[x][y].tab[i]=C[x][y].tab[i+1];
   }
-  (*C)[x][y].nbc--;
-}  
-               
-         
- void fermerCase(int *G[][9], Cand *C[][9], Case coor,int nb){
+  C[x][y].nbc--;
+}
+
+void fermerCase(int G[9][9], Cand C[9][9], Case coor,int nb){ // coor c'est une case du tableau de O 
   int i, part, j;
-  Case coor2; //autre structure de coordonnées de cases
-  coor2.x=0;
-  coor2.y=0;
-  *G[coor.x][coor.y]=nb;
-  (*C)[coor.x][coor.y].nbc=0;
-  (*C)[coor.x][coor.y].tab=NULL;
+  G[coor.x][coor.y]=nb;
+  C[coor.x][coor.y].nbc=0;
+  C[coor.x][coor.y].tab=NULL;
   for(i=0;i<9;i++){
     if(appartient(C[9][9], i, coor.y, nb)==1){
-      suppr(*C[9][9], i, coor.y, nb);  /*fonction a faire : supprime la case du nombre dans le tableau et décale vers la 
-                                        droite c'elle après et fait nbc-1*/
+      suppr(C[9][9], i, coor.y, nb);  //fonction a faire : supprime la case du nombre dans le tableau et décale vers la droite c'elle après et fait nbc-1
     }
   }
   for(i=0;i<9;i++){
     if(appartient(C[9][9], coor.x, i, nb)==1){
-      suppr(*C[9][9], coor.x, i, nb);
+      suppr(C[9][9], coor.x, i, nb);
     }
   }
   part=quelleZone(coor.y, coor.x);
@@ -277,7 +294,41 @@ Case rechCaseUnique(Cand C[][9]){
   for (i=0;i<3;i++){
     for(j=0;j<3;j++){
       if(appartient(C[9][9], coor.x + i, coor.y + j, nb)==1){
-        suppr(*C[9][9], coor.x + i, coor.y + j, nb);
+        suppr(C[9][9], coor.x + i, coor.y + j, nb);
       }
     }
   }
+}
+
+int fermerGrille(int G[9][9], Cand C[9][9], Case O[81], int *NBO)
+{
+    int fin, i, j, k, br; //dif permet de savoir si la grille est finie
+    fin=0;
+    while(fin==0)
+    {
+        br=0;  //permet de savoir si il faut revenir au while
+        for(i=0;i<*NBO;i++)
+        {
+            for(j=1;j<10;j++)  //test pour chaque cas possible
+            {
+                if (estCandUnique(C[9][9], O[i].x, O[i].y, j)==1)
+                {
+                    fermerCase(G[9][9], C[9][9], O[i], j);
+                    for(k=i;k<*NBO+1;i++)
+                    {
+                        O[k]=O[k+1]; //déplace les élément de O 
+                    }
+                    *NBO--;
+                    br=1;
+                    break;
+                }
+            }
+            if(br==1) break;
+        }
+    if(br==0) fin=1; // si il n'y a pas eu de case remplit, la grille est finie
+    }   
+    if(NBO>0) return(1); //dit que la grille est difficile
+    else return(0); //dit que la grille est facile
+    
+}
+
