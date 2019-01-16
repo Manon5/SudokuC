4f404e36d37faddad0b4c649b64d8865d093317d
FLedeux <44844255+FLedeux@users.noreply.github.com>
2019-01-16 23:06:26 +0100

    Update estCandUnique2.c

--- estCandUnique2.c
+++ estCandUnique2.c
@@ -1,5 +1,5 @@
-int estCandUnique(Cand C[9][9], Case[CA], int nb){
-    if((C[x][y].nbc!=1)||(Case[].tab[0]!=nb))
+int estCandUnique(Cand C[9][9], Case CA, int nb){
+    if((C[x][y].nbc!=1)||(C[CA.x][CA.y].tab[0]!=nb))
     return(0);
     else return(1);
 }
