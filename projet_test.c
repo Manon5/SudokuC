d5763f9680e7492f1df4634dcaaced9395ff562d
FLedeux <44844255+FLedeux@users.noreply.github.com>
2019-01-16 23:11:26 +0100

    Update projet_test.c

--- projet_test.c
+++ projet_test.c
@@ -229,7 +229,7 @@ int appartient(Cand C[9][9], int x, int y, int nb){
   return(0);
 }
 int estCandUnique(Cand C[9][9], Case CA, int nb){
-    if((C[x][y].nbc!=1)||(C[CA.x][CA.y].tab[0]!=nb))
+    if((C[CA.x][CA.y].nbc!=1)||(C[CA.x][CA.y].tab[0]!=nb))
     return(0);
     else return(1);
 }
