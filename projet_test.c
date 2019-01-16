5278f54c4ea3efe001d036d4dc563dbc2c05fb71
FLedeux <44844255+FLedeux@users.noreply.github.com>
2019-01-16 23:14:38 +0100

    Update projet_test.c

--- projet_test.c
+++ projet_test.c
@@ -321,3 +321,61 @@ void suppr(Cand C[9][9], int x, int y, int nb ){
   }
   C[x][y].nbc--;
 }
+
+void ecrireCand(Cand C[9][9])
+{
+    int a,i,j,k,l,comptb;
+    for(a=0;a<9;a++)
+    {
+        if(a%3==0)
+        { 
+            for(i=0;i<82;i++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+        else
+        {
+            printf("*");
+            for(i=0;i<3;i++)
+            {
+                for(j=0;j<26;j++)
+                {
+                    printf("-");
+                }
+                printf("*");
+            }   
+        }
+        printf("*");
+        for(k=0;k<9;k++)
+        {
+            printf(" ");
+            comptb=6-C[a][k].nbc;
+            for(l=0;l<comptb;l++)
+            {
+                printf(" ");
+            }
+            for(l=0;l<C[a][k].nbc;l++)
+            {
+                printf("%d",C[a][k].tab[l]);
+            }
+            printf(" ");
+            if(k%3==2)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf("|");
+            }
+        
+        }    
+        
+    }
+    for(i=0;i<82;i++)
+            {
+                printf("*");
+            }
+            printf("\n");    
+}
